aeb59adca8bd90960f7668970b9101baf4a10eb4
Christian Glaser <christian.glaser@uci.edu>
2021-04-26 09:36:04 +0000

    fix versioning

--- NuRadioMC/__init__.py
+++ NuRadioMC/__init__.py
@@ -1,4 +1,7 @@
 """ NuRadioMC: Simulating the radio emission of neutrinos from interaction to detector"""
-
+import sys
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(dir_path, '..'))
 import NuRadioReco
 __version__ = NuRadioReco.__version__
