fc558383999d65e9542b729ebf2b87e4a78c87be
Christian Glaser <christian.glaser@uci.edu>
2021-04-26 09:09:37 +0000

    better versioning, use version from NuRadioReco for MC

--- NuRadioMC/__init__.py
+++ NuRadioMC/__init__.py
@@ -1,3 +1,4 @@
 """ NuRadioMC: Simulating the radio emission of neutrinos from interaction to detector"""
 
-__version__ = "2.0.0"
+import NuRadioReco
+__version__ = NuRadioReco.__version__
---
+++ NuRadioReco/__init__.py
@@ -0,0 +1,3 @@
+""" NuRadioReco: Reconstruction and detector simulation framwork for radio detectos of neutrinos and cosmic rays"""
+
+__version__ = "2.0.0"
